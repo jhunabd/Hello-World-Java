c4cba39338a075216e303d68588181ecba4b17b5
Ceyhun <jhunabd@users.noreply.github.com>
2021-03-05 15:41:54 +0400

    changes for testing

--- docker-git-hello-world/src/main/java/org/pictolearn/docker/HelloWorldPing.java
+++ docker-git-hello-world/src/main/java/org/pictolearn/docker/HelloWorldPing.java
@@ -4,7 +4,8 @@ public class HelloWorldPing {
 	
 	public static void main(String args[]) throws Exception{
 		for(int i = 0 ; i < 100; i++){
-			System.out.println("Hello World Ping " + i );
+			System.out.println("Hi World Ping " + i );
+			System.out.println("Ceyhun's repository");
 			Thread.sleep(1000);
 		}
 	}
